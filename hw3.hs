524296217815379e3a4e5ddc6a62f53155476d5d
narayank97 <narayankarun@gmail.com>
2018-11-29 21:18:36 -0800

    Create hw3.hs

---
+++ hw3.hs
@@ -0,0 +1,4 @@
+reduplicates::(Eq mylist)=>[mylist]->[mylist]
+reduplicates[]= []
+reduplicates [x] = [x]
+reduplicates(myhead:mytail) | myhead `elem` mytail = reduplicates mytail|otherwise = myhead : reduplicates mytail
