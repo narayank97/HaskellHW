3ac393d017c98dbd3b9301b061a38e795901cc98
narayank97 <narayankarun@gmail.com>
2018-11-30 15:59:10 -0800

    Update hw3.hs

--- hw3.hs
+++ hw3.hs
@@ -3,3 +3,9 @@ reduplicates[]= []
 reduplicates [x] = [x]
 reduplicates(myhead:mytail) | myhead `elem` mytail = reduplicates mytail
                             |otherwise = myhead : reduplicates mytail
+                            
+intersection::(Eq mylist)=>[mylist]->[mylist]->[mylist]
+intersection[] list2= []
+intersection(myhead:mytail) list2 | myhead `elem` list2 = myhead:intersection mytail list2
+                            |otherwise = intersection mytail list2
+
