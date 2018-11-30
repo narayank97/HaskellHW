daf99e56ed575fb6c6bab0cea068598d1b8565b8
narayank97 <narayankarun@gmail.com>
2018-11-29 21:19:46 -0800

    Update hw3.hs

--- hw3.hs
+++ hw3.hs
@@ -1,4 +1,5 @@
 reduplicates::(Eq mylist)=>[mylist]->[mylist]
 reduplicates[]= []
 reduplicates [x] = [x]
-reduplicates(myhead:mytail) | myhead `elem` mytail = reduplicates mytail|otherwise = myhead : reduplicates mytail
+reduplicates(myhead:mytail) | myhead `elem` mytail = reduplicates mytail
+                            |otherwise = myhead : reduplicates mytail
