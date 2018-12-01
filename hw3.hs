a5a0bdc2015f94a88e1a2c8aec569f7ac54a5046
narayank97 <narayankarun@gmail.com>
2018-11-30 16:29:18 -0800

    Update hw3.hs

--- hw3.hs
+++ hw3.hs
@@ -8,4 +8,9 @@ intersection::(Eq mylist)=>[mylist]->[mylist]->[mylist]
 intersection[] list2= []
 intersection(myhead:mytail) list2 | myhead `elem` list2 = myhead:intersection mytail list2
                             |otherwise = intersection mytail list2
+                            
+mylast::(Eq mylist)=>[mylist]->[mylist]
+mylast []=[]
+mylast (myhead:mytail) |null mytail = myhead:mylast mytail
+                       |otherwise = mylast mytail --works but the [] or "" doesn't
 
