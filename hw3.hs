cff5e15ae52ff2575e8e0481f09c326316f5c791
narayank97 <narayankarun@gmail.com>
2018-12-02 15:39:24 -0800

    Update hw3.hs

--- hw3.hs
+++ hw3.hs
@@ -1,3 +1,4 @@
+myremoveduplicates::(Eq mylist)=>[mylist]->[mylist]
 myremoveduplicates mylist
   |null mylist = []
   |(head mylist) `elem` (tail mylist) = myremoveduplicates (tail mylist) 
@@ -9,6 +10,7 @@ myremoveduplicates_pm [x] = [x]
 myremoveduplicates_pm(x:xs) | x `elem` xs = myremoveduplicates_pm xs
                             |otherwise = x : myremoveduplicates_pm xs
 
+myintersection::(Eq mylist)=>[mylist]->[mylist]->[mylist]
 myintersection list1 list2
   | null list2 = []
   | null list1 = []
@@ -20,6 +22,7 @@ myintersection_pm[] list2= []
 myintersection_pm(x:xs) list2 | x `elem` list2 = x:myintersection_pm xs list2
                             |otherwise = myintersection_pm xs list2
 
+mylast::(Eq mylist)=>[mylist]->[mylist]
 mylast mylist
   |null mylist = []
   |null (tail mylist) = (head mylist):mylast (tail mylist)
@@ -29,11 +32,13 @@ mylast_pm::(Eq mylist)=>[mylist]->[mylist]
 mylast_pm []=[]
 mylast_pm (x:xs) |null xs = x:mylast_pm xs
                        |otherwise = mylast_pm xs --works but the [] or "" doesn't
-                      
+
+myreverse :: [a] -> [a]                      
 myreverse l
   | null l = []
   | otherwise = _myreverse l []
-    where 
+    where
+    _myreverse :: [a] -> [a] -> [a]
     _myreverse l1 l2
       |null l1 = l2
       |otherwise = _myreverse (tail l1) ((head l1):l2)
@@ -44,7 +49,9 @@ myreverse_pm l = _myreverse_pm l []
         _myreverse_pm :: [a] -> [a] -> [a]
         _myreverse_pm [] l = l
         _myreverse_pm (x:xs) l = _myreverse_pm xs (x:l) 
-        
+
+
+myreplaceall ::(Eq a) => a -> a -> [a] -> [a]          
 myreplaceall a b mylist
   |null mylist = []
   |b == (head mylist) = a:myreplaceall a b (tail mylist)
